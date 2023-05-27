7d1c1e20b42f7c88fa83d1eb8a62fbeb228f9b3d
Connor Dillon <connor@codewithconnor.com>
2023-05-27 15:05:07 -0700

    Bring app to state where it can be loaded into chrome as extension
    Co-authored-by: Cassidy <mimikomp@users.noreply.github.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './build.css';
 import App from './App.js';
-import './dist.css';
 
 const root = createRoot(document.getElementById('root'));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
-const path = require('path');
-const HTMLWepackPlugin = require('html-webpack-plugin');
+import path from 'path';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin'
 
-module.exports = {
+const __dirname = path.resolve()
+
+const config = {
   mode: process.env.NODE_ENV,
   entry: './frontend/src/index.js',
   output: {
-    path: path.join(__dirname, '/build'),
+    path: path.join(__dirname, 'build'),
     filename: 'bundle.js'
   },
   plugins: [
-    new HTMLWepackPlugin({
-      template: './frontend/src/index.html'  // not sure about this guy
-    })
+    new HTMLWebpackPlugin({
+      template: './frontend/src/index.html'
+    }),
+    new CopyPlugin({
+            patterns: [
+        { from: "./frontend/src/manifest.json", to: "./manifest.json" },
+        { from: "./frontend/favicon", to: "./favicon" },
+            ],
+        }),
   ],
   module: {
-
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s?css$/i,
+        exclude: [/node_modules/, /^index\.css$/],
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx', '.css'],
   }
 };
 
-
-//build folder goes in to chrome extension
+export default config
