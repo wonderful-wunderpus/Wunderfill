d0693514e9e69abfbf5af0c42480c71b22e9d01d
Julian <julmacalalag@gmail.com>
2023-05-27 15:12:58 -0500

    Added createuser
    Co-authored-by: Nathan <nathanhchong@users.noreply.github.com>
    Co-authored-by: wexy021 <wexy021@users.noreply.github.com>

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,28 @@
 import { Express } from "express";
-import userModel from "models/userModel"
+import User from "models/userModel"
 
+const userController = {};
 
+// create user
+userController.createUser = async (req, res, next) => {
+
+  const { username, password } = req.body;
+
+  try {
+    
+    if (!username || !password) return next(); // prob redirect or send error here
+
+    const createdUser = await User.create({username, password});
+    res.locals.user = createdUser;
+    // maybe add jwt here or something
+    return next()
+
+  } catch(err) {
+
+    next(err);
+
+  }
+
+}
+
+export default userController;
