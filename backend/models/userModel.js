1f104679b17ed35387529cfe245e38475e75680e
Julian <julmacalalag@gmail.com>
2023-05-27 14:39:24 -0500

    Updated userModel
    Co-authored-by: Nathan <nathanhchong@users.noreply.github.com>

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,13 +3,49 @@ const bcrypt = require('bcryptjs');
 
 const SALT_WORK_FACTOR = 10;
 
-//user schema
+const Schema = mongoose.Schema;
 
-//resume schema as own file
+//user schema
+const userSchema = new Schema({
+  email: String,
+  username: String,
+  password: String,
+  resume: {
+    type: Schema.Types.ObjectId,
+    ref: 'resume'
+  }
+});
 
 // userSchema.pre: (hash password)
+userSchema.pre('save', async function(next) {
+  try {
+    this.password = await bcrypt.hash(this.password, SALT_WORK_FACTOR);
+    return next();
+  } catch(err) {
+    console.log(err);
+  }
+})
+
+const User = mongoose.model('user', userSchema);
+
+//resume schema as own file
+const resumeSchema = new Schema({
+  // resumeSchemaId: {type: Number},
+  firstName: String,
+  lastName: String,
+  fields: [
+    {
+      key: 'Key',
+      value: 'Value'
+    }
+  ],
+  // address: 
+});
+
+const Resume = mongoose.mode('resume', resumeSchema);
 
 //export 
 module.exports = {
-
-};
+  User,
+  Resume
+}
