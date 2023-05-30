0352855ff86345b17efb7c48622b9f2a1a6dc85f
Nathan Chong <nathanchong@Nathans-Air.attlocal.net>
2023-05-30 01:11:51 -0700

    updated routes + server.js re cookies

---
+++ backend/models/sessionModel.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const sessionSchema = new Schema ({
+    cookieId: {type: String, required: true, unique: true},
+    createdAt: {type: Date, expires: 60, default: Date.now}
+});
+
+module.exports = mongoose.model('Session', sessionSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
 import cookieController from '../controllers/cookieController.js';
+import sessionController from '../controllers/sessionController.js'
 
 const router = express.Router();
 
@@ -8,10 +9,10 @@ const router = express.Router();
 router.get('/', (req, res) => res.status(200));
 
 // Store user to db
-router.post('/signup', userController.createUser, cookieController.setCookie, (req, res) => res.status(201).json(res.locals.user));
+router.post('/signup', userController.createUser, cookieController.setCookie, sessionController.startSession, (req, res) => res.status(201).json(res.locals.user));
 // router.get('/signup', userController, (req, res) => res.status(200));
 
 // Redirect to user info
-router.post('/login', userController.verifyUser, cookieController.setCookie, (req, res) => res.sendStatus(201));
+router.post('/login', userController.verifyUser, cookieController.setCookie, sessionController.startSession, (req, res) => res.sendStatus(201));
 
 export default router;
